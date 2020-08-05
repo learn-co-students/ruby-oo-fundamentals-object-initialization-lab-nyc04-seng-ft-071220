e0c4882d6b4e6ad59a03ebb55d65246c9d2533a3
hortencia c <hortenciaecisneros@gmail.com>
2020-08-05 00:22:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+end
+
+
+# define a "Dog class" "initialize" thats takes an argument for the dogs "name" and breed and set breed in arg(breed ="Mutt") if none is provided
+# create a @name = name @breed = breed
